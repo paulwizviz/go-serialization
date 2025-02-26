e997387f2580e80ff5188272b736e502ebcd2298
Paul Sitoh <paulwizviz@gmail.com>
2025-02-26 08:37:11 +0000

    Update cbor package
    - Fix interface example to ensure that cbor tags are added to address.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- cborser/doc.md
+++ cborser/doc.md
@@ -4,7 +4,15 @@ This section demonstrates technique for serialising Go data types to CBOR
 
 ## Working Examples
 
-* [Types](./types_test.go) - serialization of primitive types
+1. Primitive types
+    * Examples showing the result of serialising primitive types
+    * [Source](./types_test.go)
+
+2. Struct types
+    * Examples demonstrating the result of serializing interface and struct types
+    * [Source](./struct_test.go)
+
+
 
 ## References
 
--- cborser/struct_test.go
+++ cborser/struct_test.go
@@ -70,6 +70,12 @@ func (d human) Address() address {
 	return d.address
 }
 
+type auxAddr struct {
+	HouseNum int    `cbor:"housenum"`
+	Road     string `cbor:"road"`
+	Country  string `cbor:"country"`
+}
+
 func (d *human) MarshalCBOR() ([]byte, error) {
 
 	bday := d.birthday.Format(time.RFC3339)
@@ -78,12 +84,16 @@ func (d *human) MarshalCBOR() ([]byte, error) {
 		Firstname string  `cbor:"firstname"`
 		Surname   string  `cbor:"surname"`
 		Birthday  string  `cbor:"birthday"`
-		Address   address `cbor:"address"`
+		Address   auxAddr `cbor:"address"`
 	}{
 		Firstname: d.firstname,
 		Surname:   d.surname,
 		Birthday:  bday,
-		Address:   d.address,
+		Address: auxAddr{
+			HouseNum: d.address.HouseNum,
+			Road:     d.address.Road,
+			Country:  d.address.Country,
+		},
 	})
 }
 
@@ -92,7 +102,7 @@ func (d *human) UnmarshalCBOR(data []byte) error {
 		Firstname string  `cbor:"firstname"`
 		Surname   string  `cbor:"surname"`
 		Birthday  string  `cbor:"birthday"`
-		Address   address `cbor:"address"`
+		Address   auxAddr `cbor:"address"`
 	}
 	if err := cbor.Unmarshal(data, &aux); err != nil {
 		return err
@@ -104,7 +114,9 @@ func (d *human) UnmarshalCBOR(data []byte) error {
 		return err
 	}
 	d.birthday = bday
-	d.address = aux.Address
+	d.address.HouseNum = aux.Address.HouseNum
+	d.address.Road = aux.Address.Road
+	d.address.Country = aux.Address.Country
 	return nil
 }
 
@@ -121,7 +133,7 @@ func EmptyHuman() Human {
 	return &human{}
 }
 
-func Example_unmarshalInterfaceCBOR() {
+func Example_interfaceCBOR() {
 
 	bday := time.Date(2025, time.April, 1, 0, 0, 0, 0, time.Local).UTC()
 
@@ -131,7 +143,7 @@ func Example_unmarshalInterfaceCBOR() {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("1. %x\n", b)
+	fmt.Printf("1. %v %x\n", h, b)
 
 	h1 := EmptyHuman()
 	err = cbor.Unmarshal(b, &h1)
@@ -145,7 +157,7 @@ func Example_unmarshalInterfaceCBOR() {
 	fmt.Printf("2.4 %v\n", h1.Address())
 
 	// Output:
-	// 1. a46946697273746e616d65646a6f686e675375726e616d6563646f6568426972746864617974323032352d30332d33315432333a30303a30305a6741646472657373a368486f7573654e756d0164526f616464726f616467436f756e747279625347
+	// 1. &{john doe {0 63879058800 <nil>} {1 road SG}} a46966697273746e616d65646a6f686e677375726e616d6563646f6568626972746864617974323032352d30332d33315432333a30303a30305a6761646472657373a368686f7573656e756d0164726f616464726f616467636f756e747279625347
 	// 2.1 john
 	// 2.2 doe
 	// 2.3 2025-03-31T23:00:00Z
